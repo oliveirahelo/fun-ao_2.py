2d9d8e6f75410cfc894d3b84f856b705b47f8116
oliveirahelo <heloisa.oli15@gmail.com>
2025-05-28 15:19:08 -0300

    Add files via upload

---
+++ while.py
@@ -0,0 +1,10 @@
+def tabuada():
+    numero = int(input(" Digite seu numero: "))
+    comeco = int(input("Digite o numero que devera começar: "))
+    fim = int(input(" Digite ate qual numero deverar ser multiplicado: "))
+    
+    while comeco <= (fim):
+        print(f" {comeco} x {numero} = {comeco * numero}")
+        comeco+=1
+        
+tabuada()   
